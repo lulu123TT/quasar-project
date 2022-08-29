92ba63e7ca4078a79bc038345337a9b6ba4578a9
lulu123TT <3287776797@qq.com>
2022-08-29 16:14:39 +0800

    first commit

---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="q-pa-md q-gutter-md fixed-center">
+    <q-card class="my-card q-pa column justify-center" style="min-width: 300px;min-height: 300px;">
+      <q-card-section>
+        <div class="text-h5">Register</div>
+      </q-card-section>
+      <q-card-section>
+        <q-input v-model="text" label="Username"/>
+        <q-input v-model="text" label="Password"/>
+        <q-input v-model="text" label="Email"/>
+        <q-input v-model="text" label="Verification Code"/>
+      </q-card-section>
+      <q-card-section class="row justify-center">
+        <q-btn color="primary" label="NEXT" />
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const  text = ref('')
+</script>
+
+<style scoped lang="sass">
+.my-card
+  max-width: 340px
+</style>
